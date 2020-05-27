41defe1703a50735d7d313e0a5dd33ab2e0778de
AnonymousUser <deeksha.sharma35@t-mobile.com>
2020-05-27 16:52:47 -0700

    added custom input elements. added dynamic form element rendering and input field validation. added submit functionality to submit order to server.

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import classes from './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+    const inputClasses = [classes.InputElement]
+    let validationError = null;
+    if (props.invalid && props.shouldValidate && props.touched) { 
+        inputClasses.push(classes.Invalid); 
+        validationError = <p className={classes.ErrorMsg}>{props.errorMsg}</p>
+    }
+
+    switch (props.eleType) {
+        case ('input'):
+            inputElement = <input 
+                className={inputClasses.join(" ")} 
+                {...props.eleConfig}
+                value={props.value}
+                onChange={props.changed}
+            />
+            break;
+        case ('textarea'):
+            inputElement = <textarea 
+                className={inputClasses.join(" ")} 
+                {...props.eleConfig}
+                value={props.value}
+                onChange={props.changed}
+            />
+            break;
+        case ('select'):
+                inputElement = (
+                    <select 
+                        className={inputClasses.join(" ")} 
+                        value={props.value}
+                        onChange={props.changed}
+                    >
+                        {props.eleConfig.options.map(option => (
+                            <option 
+                                key={option.value} 
+                                value={option.value}
+                            >{option.label}</option>
+                        ))}
+                    </select>
+                );
+                break;
+        default:
+            inputElement = <input 
+                className={inputClasses.join(" ")} 
+                {...props.eleConfig}
+                value={props.value}
+            />
+            break;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+            {validationError}
+        </div>
+    );
+};
+
+export default input;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,34 +3,111 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            zipCode: ''
+        orderForm: {
+            name: {
+                eleType: "input",
+                eleConfig: {
+                    type: "text",
+                    placeholder: "Your Name"
+                },
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                errorMsg: "Please enter a value!",
+            },
+            street: {
+                eleType: "input",
+                eleConfig: {
+                    type: "text",
+                    placeholder: "Street"
+                },
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                errorMsg: "Please enter a value!",
+            },
+            zipCode: {
+                eleType: "input",
+                eleConfig: {
+                    type: "text",
+                    placeholder: "ZipCode"
+                },
+                value: "",
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                },
+                valid: false,
+                touched: false,
+                errorMsg: "Please enter a value 5 chars in length!",
+            },
+            country: {
+                eleType: "input",
+                eleConfig: {
+                    type: "text",
+                    placeholder: "Country"
+                },
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                errorMsg: "Please enter a value!",
+            },
+            email: {
+                eleType: "input",
+                eleConfig: {
+                    type: "email",
+                    placeholder: "Your email"
+                },
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                errorMsg: "Please enter a valid email!",
+            },
+            deliveryMethod: {
+                eleType: "select",
+                eleConfig: {
+                    options: [
+                        {value: 'fastest', label: 'Fatest Delivery'},
+                        {value: 'cheapest', label: "Cheapest Delivery"}
+                    ]
+                },
+                value: "fastest",
+                validation: {},
+                valid: true,
+            },
         },
+        formIsValid: false,
         loading: false,
     }
 
     orderHandler = (event) => {
         event.preventDefault(); // prevent reload when form submitted
         this.setState({loading: true});
+        const formData = {};
+        for (let formEle in this.state.orderForm) {
+            formData[formEle] = this.state.orderForm[formEle].value;
+        }
         const data = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
-            customer: {
-                name: "Deeksha Sharma",
-                address: {
-                   street: "1 Test streed",
-                   zipCode: "45675",
-                   country: "United States"
-                },
-                email: "test@test.com",
-            },
-            deliveryMethod: "Expedited shipping"
+            price: this.props.price,
+            orderData: formData
         }
         axios.post("/orders.json", data)
             .then(response => { 
@@ -45,16 +122,73 @@ class ContactData extends Component {
             })
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangeHandler = (event, inputID) => {
+        // this alone will not do a deep clone because there are nested objects inside orderForm those will just have their pointers stored so changes to those objects will be mutable
+        //const updatedForm = { ...this.state.orderForm }
+
+        const updatedForm = {...this.state.orderForm};
+        const updatedFormEle = {...updatedForm[inputID]};
+        // if you are also updating the eleConfig nested object you would need to clone that deeply also 
+        // const updateFormEleConfig = {...updatedFormEle.eleConfig}
+
+        updatedFormEle.value = event.target.value;
+        updatedFormEle.valid = this.checkValidity(updatedFormEle.value, updatedFormEle.validation);
+        updatedFormEle.touched = true;
+        updatedForm[inputID] = updatedFormEle;
+
+        let formIsValid = true;
+        for (let id in updatedForm) {
+            formIsValid = updatedForm[id].valid && formIsValid;
+        }
+
+        this.setState({
+            orderForm: updatedForm, 
+            formIsValid: formIsValid
+        });
+    }
+
     render () {
+        const formEleArray = [];
+        for (let key in this.state.orderForm) {
+            formEleArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
-            <form>
-                <input type="text" name="name" placeholder="Your name" />
-                <input type="email" name="email" placeholder="Your email" />
-                <input type="text" name="street" placeholder="Your street" />
-                <input type="text" name="zip" placeholder="Your zip code" />
+            <form onSubmit={this.orderHandler}>
+                {formEleArray.map(formEle => (
+                    <Input 
+                        key={formEle.id}
+                        eleType={formEle.config.eleType}
+                        eleConfig={formEle.config.eleConfig}
+                        value={formEle.config.value}
+                        invalid={!formEle.config.valid}
+                        shouldValidate={formEle.config.validation}
+                        touched={formEle.config.touched}
+                        errorMsg={formEle.config.errorMsg}
+                        changed={(event) => {this.inputChangeHandler(event, formEle.id)}}
+                    />
+                ))}
                 <Button 
                     buttonType="Success" 
-                    clicked={this.orderHandler}
+                    disabled={!this.state.formIsValid}
                 >Order</Button>
             </form>
         );
